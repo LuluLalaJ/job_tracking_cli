2bd901b38568cc74170f5c439313cec8e538f4ea
Chen <chen.jiang.1206@gmail.com>
2023-05-15 09:57:58 -0400

    add migration versions + db

---
+++ lib/db/migrations/versions/3d464c8668b2_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 3d464c8668b2
+Revises: 
+Create Date: 2023-05-15 09:26:57.571019
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3d464c8668b2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
